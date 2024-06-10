25ae996f38aa7153f0ab93729ec6f8ad334b3b05
marcello <mlaferreira.marcello@gmail.com>
2024-06-10 14:42:05 -0300

    escrevendo o html e o css do depoiementos e o seu js

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,5 +19,49 @@ menuOpener.addEventListener('click' , (e) =>{
         
     }
 
+});
 
-})
+// TESTIMONIALS
+let testimonials = [
+    { quote: '"Mais do que nunca, os animais de estimação são tratados como membros da familia."', origin: 'cbs.svg'},
+    { quote: '"DogDev é um serviço de entrega direto ao consumidor, preparado na hora com ingredientes 100% reais, Ingredientes que os humanos reconheceriam."', origin: 'forbes.svg'},
+    { quote: '"DogDev usa ingredientes simples e limpos em seus pratos."', origin: 'fox.svg'},
+    { quote: '"Vejo você[Marcelo] como um verdadeiro herói"', origin: 'sharktank.svg'}
+];
+let testimonialQuote = document.querySelector('.testimonials .quote');
+let testimonialIcons = document.querySelector('.testimonials .icons');
+
+for(let tindex in testimonials){
+    let img = document.createElement('img');
+    img.setAttribute('src', './assets/images/' + testimonials[parseInt(tindex)].origin);
+    img.style.cursor = 'pointer';
+    img.addEventListener('click', (e) => {
+        e.preventDefault();
+        fillTestimonial(parseInt(tindex));
+
+    });
+    testimonialIcons.appendChild(img);
+}
+let currentTestimonail = 0;
+let testimonialTimer;
+
+const fillTestimonial = (index) => {
+    clearTimeout(testimonialTimer);
+    currentTestimonail = index;
+    testimonialQuote.innerHTML = testimonials[currentTestimonail].quote;
+    let imgs = testimonialIcons.querySelectorAll('img');
+    for(let img of imgs){
+        img.style.opacity = .2;
+    };
+    imgs[currentTestimonail].style.opacity = 1;
+    testimonialTimer = setTimeout((nextTestimonial), 3000);
+};
+
+const nextTestimonial = () => {
+    if(testimonials[currentTestimonail + 1]){
+        fillTestimonial(currentTestimonail + 1);
+    }else{
+        fillTestimonial(0);
+    }
+}
+nextTestimonial();
